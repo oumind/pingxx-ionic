2d9f6e611ee82d79490aeac59c513203d1acc0ce
张衡 <zhangheng@uvoplus.com>
2015-05-29 17:28:25 +0800

    use ionic finish a ng-cordova-pingxx demo

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,62 @@
-// Ionic Starter App
+(function() {
+  'use strict';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+  var app = angular.module('pingxx.ionic', ['ionic', 'restangular', 'ngCordova.plugins.pingxx']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  app.constant('ApiEndpoint', {
+    api_url: 'http://10.246.40.125:8010'
   });
-})
+
+  app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  });
+
+  app.config(function(RestangularProvider, ApiEndpoint) {
+    RestangularProvider.setBaseUrl(ApiEndpoint.api_url);
+  });
+
+  app.controller('MainController', function ($scope, $rootScope, cordovaPingxx, Restangular, $ionicPopup) {
+    $scope.amount = 0.1;
+    $scope.channel = {name: 'alipay'};
+
+
+    $scope.pay = function () {
+      var data = {
+        "channel": $scope.channel.name,
+        "amount": $scope.amount*100
+      }
+
+      Restangular.all('').customPOST(data, 'pay').then(function(charge) {
+        pingxx.createPayment(charge, 'pingxxionic');
+      }, function(error) {
+        $ionicPopup.alert({
+          title: '网络错误',
+          template: 'ajax请求失败'
+        })
+      });
+    };
+
+    $rootScope.$on('pingxx:pay-finished', function (event, data) {
+      $scope.payResult = {
+        "result": data.result,
+        "errorCode": data.errorCode,
+        "errorMsg": data.errorMsg
+      }
+
+      $ionicPopup.alert({
+        scope: $scope,
+        title: 'pay result',
+        templateUrl: 'pay-result-dialog.html'
+      })
+    });
+  });
+}());
